ed62027cd61db63e31080863dc2fd793e6fc2048
Guilherme Koller <g_koller@hotmail.com>
2021-05-22 16:40:34 -0300

    image load and rendering

--- main.py
+++ main.py
@@ -10,10 +10,23 @@ WHITE = (255,255,255)
 
 FPS = 60
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load("Assets/")
+SPACESHIP_HEIGHT, SPACESHIP_WIDHT = 40, 55
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(SPACESHIP_WIDHT,SPACESHIP_HEIGHT)),90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDHT,SPACESHIP_HEIGHT)),-90)
+
+BACKGROUND = pygame.image.load(os.path.join('Assets', 'space.png'))
+BACKGROUND = pygame.transform.scale(BACKGROUND,(WIDTH,HEIGHT))
+
+
 
 def draw_window():
-    WIN.fill(WHITE)
+    WIN.blit(BACKGROUND, (0,0))
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (0,0))
+    WIN.blit(RED_SPACESHIP_IMAGE,(300,300))
     pygame.display.update()
 
 
